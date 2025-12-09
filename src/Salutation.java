af1935d9bd0dc16289434693ff001cdc46385cb0
Souissi Ahmed <ahmed.souissi2514@gmail.com>
2025-12-09 16:26:01 +0100

    correction

--- src/Salutation.java
+++ src/Salutation.java
@@ -1,8 +1,7 @@
 public class Salutation {
     public static void main(String[] args) {
-        System.out.println("Bonjour de Thibault ODOR !");
-        System.out.println("Hello, user!");
-        System.out.println("Hello, I'm Souissi Ahmed!");
+      System.out.println("Hello, I'm Souissi Ahmed!");  
+      System.out.println("Hello, user!");
 
     }
 }
