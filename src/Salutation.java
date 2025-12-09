8fe9a8890a63004e61de7490005b2a2b7a2c541c
Wael Saidane <wsaidane@example.com>
2025-12-09 16:30:14 +0100

    Ajout de Salutation

--- src/Salutation.java
+++ src/Salutation.java
@@ -1,7 +1,6 @@
 public class Salutation {
     public static void main(String[] args) {
-        System.out.println("Bonjour de la part de Julien !");
-        System.out.println("Bonjour de Thibault ODOR !");
+        System.out.println("Bonjour de la part de Wael !");
         System.out.println("Hello, user!");
     }
 }
