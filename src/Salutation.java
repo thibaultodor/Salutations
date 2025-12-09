c323c868dbc90c79b4ee090274bb48f076a69332
Souissi Ahmed <ahmed.souissi2514@gmail.com>
2025-12-09 16:19:01 +0100

    salutation de ahmed

--- src/Salutation.java
+++ src/Salutation.java
@@ -2,5 +2,7 @@ public class Salutation {
     public static void main(String[] args) {
         System.out.println("Bonjour de Thibault ODOR !");
         System.out.println("Hello, user!");
+        System.out.println("Hello, I'm Souissi Ahmed!");
+
     }
 }
