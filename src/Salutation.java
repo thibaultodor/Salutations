b1018441de3f1ea3f8de718a88f2e59d9a14207b
Souissi Ahmed <ahmed.souissi2514@gmail.com>
2025-12-09 16:35:31 +0100

    UpdateLastVersion

--- src/Salutation.java
+++ src/Salutation.java
@@ -1,13 +1,7 @@
 public class Salutation {
     public static void main(String[] args) {
-<<<<<<< HEAD
       System.out.println("Hello, I'm Souissi Ahmed!");  
-      System.out.println("Hello, user!");
-
-=======
-        System.out.println("Bonjour de la part de Julien !");
+      System.out.println("Bonjour de la part de Julien !");
         System.out.println("Bonjour de Thibault ODOR !");
         System.out.println("Hello, user!");
->>>>>>> 1c64433ad5a806583a9925954698a0bd58507168
     }
-}
